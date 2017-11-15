623724d0076f8dea2766d63155b09358e803403a
marmo002 <marmo_02@hotmail.com>
2017-11-14 19:08:52 -0500

    finished assignment

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/cohorts/67/assignments/2055/latest)
+
+STEP0
+1. What HTTP method did your browser use to make the request?
+  It used GET method.
+2. How many milliseconds did it take your browser to complete it?
+  91 ms
+3. What HTTP status code did the server return? What does that mean?
+  It returned a 200 status code, which means Ok, go ahead.
+4. Look at the "Initiator" and "Type" columns. Was this an xhr/JavaScript/AJAX request or a normal browser request?
+  Initiator was other, the Type was document.
--- ajax.js
+++ ajax.js
@@ -1,5 +1,108 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  /* Your code goes here */
+  var buttonOne = document.getElementById('buttonOne');
 
+  buttonOne.addEventListener('click', function(){
+
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/',
+      method: 'GET',
+      data: {},
+      dataType:'text',
+    });
+
+  });
+
+  var buttonTwo = document.getElementById('buttonTwo');
+
+  buttonTwo.addEventListener('click', function(){
+
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/pong',
+      method: 'GET',
+      // data: {},
+      dataType:'text'
+    }).done(function(responseData){
+      console.log('Request ended well');
+      document.getElementById('step3456').append(responseData);
+    }).fail(function(jqXHR, textStatus, errorThrown){
+      console.log('Request failed');
+      document.getElementById('step3456').append(jqXHR.responseText);
+    }).always(function(){
+      console.log('Hey, the request finished');
+    });
+
+  });
+
+  var buttonThree = document.getElementById('buttonThree');
+
+  buttonThree.addEventListener('click', function(){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+      dataType: 'TEXT',
+      data: {
+        amount: 5,
+      }
+    }).done(function(responseData){
+      console.log('resquest to count worked well');
+      document.getElementById('step7').append(responseData);
+    });
+  });
+
+  var buttonFour = document.getElementById('buttonFour');
+
+  buttonFour.addEventListener('click', function(){
+
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method: 'GET',
+      dataType: 'text',
+      data: {
+        timezone: 'Asia/Kolkata',
+      }
+    }).done(function(response){
+      console.log('You got response from /time');
+      document.getElementById('step8').append(response);
+    });
+  });
+
+  var buttonFive = document.getElementById('buttonFive');
+
+  buttonFive.addEventListener('click', function(){
+    var ulTag = document.createElement('ul');
+    ulTag.setAttribute('id', 'car_ul');
+    document.getElementById('step9').append(ulTag);
+
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/a_car',
+      method: 'GET',
+      dataType: 'HTML'
+    }).done(function(response){
+      ulTag.innerHTML = response;
+    });
+
+  });
+
+  var submitButton = document.querySelector('input[type="submit"]');
+
+  submitButton.addEventListener('click', function(event){
+    event.preventDefault();
+    var userInput = document.getElementById('timezone')
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method: 'GET',
+      dataType: 'text',
+      data: {
+        timezone: userInput.innerText,
+      }
+    }).done(function(response){
+      console.log('You got response from /time');
+      document.getElementById('step10').append(response);
+    }).fail(function(jqXHR, textStatus, errorThrown){
+
+      console.log('Request failed');
+      document.getElementById('step10').append(jqXHR.responseText);
+    });
+  });
 });
